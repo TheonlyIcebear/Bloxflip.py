b64c5e6e51b6d88ee5587e54f1474573488c0cf9
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-12-23 21:34:19 -0500

    Add files via upload

--- examples/crash_example.py
+++ examples/crash_example.py
@@ -21,7 +21,6 @@ for games in crash.crashPoints(amount=30, interval=0.01):
     current = games[0]
     history = games[1]
 
-    print(current.crash_point)
     """Attributes: 
      - crash_point
      - private_hash
@@ -31,5 +30,8 @@ for games in crash.crashPoints(amount=30, interval=0.01):
     """
 
     time.sleep(2) # Make sure bet isn't placed before game starts
+    balance = round(Currency.balance(auth), 2)
+	print(f"Balance: {balance}")
+	print(f"Games: {current.crash_point}")
     if not failed:
         websocket.join(betamount=1, multiplier=1.5) # Join the game
