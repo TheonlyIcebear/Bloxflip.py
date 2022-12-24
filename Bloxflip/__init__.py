0bda2ee2d7e43e54cf8d44469a95c9f0504993ab
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-12-24 10:39:17 -0500

    Update __init__.py

--- Bloxflip/__init__.py
+++ Bloxflip/__init__.py
@@ -1,8 +1,3 @@
-import cloudscraper, websocket, requests, base64, json, time, ssl, os
-from typing import Union, Generator, Any
-from websocket import create_connection
-from random import randbytes
-
 from utils.errors import errors
 from crash import *
 from mines import *
