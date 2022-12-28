30a2c8ecacd4cffdfbc93f26d6d46487066a7e51
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-12-28 08:51:12 -0500

    Add files via upload

--- examples/jackpot_example.py
+++ examples/jackpot_example.py
@@ -1,21 +1,10 @@
 from bloxflip import Currency, Jackpot, Authorization
-import bloxflip, time
 
-failed = False
 auth = ""
-
-if not Authorization.validate(auth):
-    print("Invalid authorization")
-    exit()
-
 jackpot = Jackpot(auth)
 
-
-try:
-    websocket = jackpot.Websocket()
-    websocket.connect()
-except:
-    failed = True
+websocket = jackpot.Websocket()
+websocket.connect()
 
 # example 1
 
@@ -25,27 +14,4 @@ for pot in jackpot.sniper(snipe_at=0.5, interval=0.01):
     print(f"Balance: {balance}")
     print(f"Pot value: {pot.value}")
 
-    """"Attributes: pot
-         - value
-         - time
-         - status
-         - winner
-         - winningColor
-         - id
-        """
-
-    if not failed:
-        websocket.join(betamount=pot.value*2)  # Join the game with a 50% of winning
-
-# example 2
-
-
-def func(pot):
-    balance = round(Currency.balance(auth), 2)
-    print(f"Balance: {balance}")
-    print(f"Pot value: {pot.value}")
-
-    if not failed:
-        websocket.join(betamount=pot.value * 2, multiplier=1.5)  # Join the game with a 50% of winning
-
-jackpot.sniper(on_game_start=func)
+    websocket.join(betamount=pot.value*2)  # Join the game with a 50% of winning
